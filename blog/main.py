faf518bdeca2f31612360fd8b57f7a600a08bd7b
duveshp <duveshp03@gmail.com>
2025-01-18 10:46:25 +0530

    proj structure built

--- blog/main.py
+++ blog/main.py
@@ -1,26 +1,43 @@
-from fastapi import FastAPI,HTTPException
-from . import schemas,models
-from .database import engine, SessionLocal
-from sqlmodel import SQLModel
+from fastapi import Depends, FastAPI
 
-# models.Base.metadata.create_all(bind=engine)
-SQLModel.metadata.create_all(engine)
+from .dependencies import get_query_token, get_token_header
+from .routers import items, users
 
 app = FastAPI()
 
-@app.post('/blog')
-def createBlog(request:schemas.OpsBlogs, session:SessionLocal) -> models.Blog2:
-    new_blog = models.Blog2(title=request.title,body=request.body,published_at=request.published_at)
-    session.add(new_blog)
-    session.commit()
-    session.refresh(new_blog)
-    return new_blog
 
-@app.get("/blog/{blog_id}")
-def read_hero(hero_id: int, session: SessionLocal) -> models.Blog2:
-    blog = session.get(models.Blog2, hero_id)
-    if not blog:
-        raise HTTPException(status_code=404, detail="Hero not found")
-    return blog
+app.include_router(users.router)
+app.include_router(items.router)
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Bigger Applications!"}
+
+# from fastapi import FastAPI,HTTPException
+# from . import schemas,models
+# from .database import engine, SessionLocal
+# from sqlmodel import SQLModel
+
+# # models.Base.metadata.create_all(bind=engine)
+# SQLModel.metadata.create_all(engine)
+
+# app = FastAPI()
+
+# @app.post('/blog')
+# def createBlog(request:schemas.OpsBlogs, session:SessionLocal) -> models.Blog2:
+#     new_blog = models.Blog2(title=request.title,body=request.body,published_at=request.published_at)
+#     session.add(new_blog)
+#     session.commit()
+#     session.refresh(new_blog)
+#     return new_blog
+
+# @app.get("/blog/{blog_id}")
+# def read_hero(hero_id: int, session: SessionLocal) -> models.Blog2:
+#     blog = session.get(models.Blog2, hero_id)
+#     if not blog:
+#         raise HTTPException(status_code=404, detail="Hero not found")
+#     return blog
 
 
